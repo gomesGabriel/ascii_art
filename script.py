1594e8fd1d38ebcd86138c92839554f3a42339be
David Sais <73711375+Grench6@users.noreply.github.com>
2021-06-26 15:52:40 -0500

    Improved for automation
    The following changes were made:
    - The user must specify a input image as an argument when calling the script.
    - The ASCII-art is written to std-out.

--- script.py
+++ script.py
@@ -1,13 +1,15 @@
 from os import path
 import PIL.Image
+import sys
 ASCII_CHARS = ['@', '#', 'S', '%', '?', '*', '+', ';', ':', ',', '.']
 
-def resize_image(image, new_width=50):
+def resize_image(image, new_width=100):
     width, heigth = image.size
-    ratio = heigth/width/1.5
+    ratio = heigth/width
     new_height = int(new_width*ratio)
-    resized_image = image.resize((new_width, new_height))
-    ##print('New w= {}, new h = {}', new_width, new_height)
+    resized_image = image.resize((new_width, int(new_height/2)))
+    #resized_image = image.resize((new_width, int(new_height/3 * 2)))
+    #resized_image = image.resize((new_width, new_height))
     return(resized_image)
 
 def grayfy(image):
@@ -19,21 +21,21 @@ def pixels_to_ascii(image):
     characters = "".join([ASCII_CHARS[pixel//25] for pixel in pixels])
     return(characters)
 
-def main (new_width=50):
-    path = input('Enter a valid path name to an image:\n ')
-    try:
-        image = PIL.Image.open(path)
-    except:
-        print(path, 'Is not a valid pathname to an image')
+def main (new_width=100):
+    if len(sys.argv) <= 1 :
+        print('You must provide one argument: [path to source image]')
+        return
     
-    ##resize_image(image)
+    try:
+        image = PIL.Image.open(sys.argv[1])
+    except:
+        print(sys.argv[1], 'Is not a valid pathname to an image')
+    
     new_image_data = pixels_to_ascii(grayfy(resize_image(image)))
 
     pixel_count = len(new_image_data)
     ascii_image = '\n'.join(new_image_data[i:(i+new_width)] for i in range(0, pixel_count, new_width))
     
     print(ascii_image)
-    
-    with open('ascii_image.txt', 'w') as f:
-        f.write(ascii_image)
+
 main()
